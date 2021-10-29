c01feb1612044bf23e9a8da3cb90e31ab976a3e4
electricessence <5899455+electricessence@users.noreply.github.com>
2021-10-28 22:01:02 -0700

    Improved coverage with most of the core complete.

--- Open.Range.Tests/RangeExtensionsTests.cs
+++ Open.Range.Tests/RangeExtensionsTests.cs
@@ -11,6 +11,25 @@ namespace Open.RangeTests;
 public static class RangeExtensionsTests
 {
 	[Fact]
+	public static void NullParam()
+	{
+		Assert.Throws<ArgumentNullException>(
+			() => RangeExtensions.Range<int>(null));
+		Assert.Throws<ArgumentNullException>(
+			() => RangeExtensions.Range<int>(null, null));
+		Assert.Throws<ArgumentNullException>(
+			() => RangeExtensions.Range((IEnumerable<int>)null, 0, null));
+		Assert.Throws<ArgumentNullException>(
+			() => RangeExtensions.Range(null, 0));
+		Assert.Throws<ArgumentNullException>(
+			() => RangeExtensions.Range((IEnumerable<int>)null, (Func<int, double>)null));
+		Assert.Throws<ArgumentNullException>(
+			() => RangeExtensions.Range(Enumerable.Empty<int>().AsParallel(), null));
+		Assert.Throws<ArgumentNullException>(
+			() => RangeExtensions.Range(Enumerable.Empty<int>(), 0, null));
+	}
+
+	[Fact]
 	public static void MinMax()
 	{
 		Enumerable.Range(5, 20)
@@ -23,7 +42,7 @@ public static class RangeExtensionsTests
 		Enumerable.Range(5, 20000)
 			.Select(e => (double)e)
 			.Prepend(double.NaN).Prepend(double.NaN)
-			.Append(double.NaN).AsParallel().Range(e=>e)
+			.Append(double.NaN).AsParallel().Range(e => e)
 			.Equals(Range.Create(5d, 20004d))
 			.Should().BeTrue();
 
